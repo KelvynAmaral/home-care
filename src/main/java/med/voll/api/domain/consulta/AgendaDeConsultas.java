fce4342baa0de70c480209704aa5fcc2fcfe421f
Kelvyn <dev.kelvynjohnson@gmail.com>
2023-11-07 15:39:03 -0300

    testar controllers

---
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -0,0 +1,84 @@
+package med.voll.api.domain.consulta;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.validacoes.agendamento.ValidadorAgendamentoDeConsulta;
+import med.voll.api.domain.consulta.validacoes.cancelamento.ValidadorCancelamentoDeConsulta;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AgendaDeConsultas {
+
+    //SOLID - D - Dependency Inversion Principle
+    //3. Principio da inversão de dependencia
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @Autowired
+    private List<ValidadorAgendamentoDeConsulta> validadores;
+    //Classe service ela depende de uma abstração "Validadores" e não de uma implementação sem necessidade de implementar a classe.
+    @Autowired
+    private List<ValidadorCancelamentoDeConsulta> validadoresCancelamento;
+    //Classe service ela depende de uma abstração "Validadores" e não de uma implementação não ha necessidade de implementar a classe.
+
+    //SOLID - O - Open/Closed Principle -
+    //2. Principio aberto/fechado
+    public DadosDetalhamentoConsulta agendar(DadosAgendamentoConsulta dados) {
+        if (!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoException("Id do paciente informado não existe!");
+        }//verifica se o paciente existe , se não existir ele manda mensagem de erro.
+
+        if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
+            throw new ValidacaoException("Id do médico informado não existe!");
+        }//verifica se o medico existe , se não existir ele manda mensagem de erro.
+
+        validadores.forEach(v -> v.validar(dados));// Pattern Strategy - executa todos os validadores.
+
+        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
+        var medico = escolherMedico(dados);
+        if (medico == null) {
+            throw new ValidacaoException("Não existe médico disponível nessa data!");
+        }
+
+        var consulta = new Consulta(null, medico, paciente, dados.data(), null);
+        consultaRepository.save(consulta);
+
+        return new DadosDetalhamentoConsulta(consulta);
+    }
+
+    public void cancelar(DadosCancelamentoConsulta dados) {
+        if (!consultaRepository.existsById(dados.idConsulta())) {
+            throw new ValidacaoException("Id da consulta informado não existe!");
+        }
+
+        validadoresCancelamento.forEach(v -> v.validar(dados));// Pattern Strategy - executa todos os validadores.
+
+        var consulta = consultaRepository.getReferenceById(dados.idConsulta());
+        consulta.cancelar(dados.motivo());
+    }
+
+    // metodo verifica se está chegando o Iddo medico
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        if (dados.idMedico() != null) {
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+
+        if (dados.especialidade() == null) {
+            throw new ValidacaoException("Especialidade é obrigatória quando médico não for escolhido!");
+        }
+
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
+    }
+
+}
